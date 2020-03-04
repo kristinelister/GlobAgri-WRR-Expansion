495e314aeac043cf474353e62e672bdda6295f8b
Kristine Lister <31804766+kristinelister@users.noreply.github.com>
2020-03-03 19:05:18 -0500

    Update fix_country_bounds_iso_numbers.py

--- Code/fix_country_bounds_iso_numbers.py
+++ Code/fix_country_bounds_iso_numbers.py
@@ -5,34 +5,30 @@ import rasterio
 import glob
 import subprocess
 from pycountry import countries
-#docker push kristinelister/basic-geo-python-docker:latest
-#docker.io/kristinelister/basic-geo-python-docker:latest
-#gcsfuse globagri-upload-bucket globagriexpansion/
-#gcr.io/globagri/basic-geo-python-docker
-#sudo umount ~/globagriexpansion/
-#gsutil -m cp ExpansionRound1.py gs://globagri-upload-bucket/Code/
 
-def convert_iso_num(data,iso_code_convert_list,out_code):
+
+def convert_iso_num(data,iso_code_convert_list, iso_table,out_code):
+    print(iso_code_convert_list)
     try:
-        iso_num_convert_list = [int(countries.get(alpha_3=x).numeric) for x in iso_code_convert_list]
+        iso_num_convert_list = [iso_table[iso_table['ISO3']==x]['ISO_NUM'].values[0] for x in iso_code_convert_list]
     except:
-        if out_code == 729:
-            iso_num_convert_list = [728,729]
-    print(iso_num_convert_list)
+        for x in iso_code_convert_list:
+            print(x)
+            print(iso_table[iso_table['ISO3']==x]['ISO_NUM'].values[0])
     data[np.isin(data,iso_num_convert_list)] = out_code
     return data
 
-
-globagri_regions = ['CHN','SUN', 'BAL','YUG','SDN'];
-SUN_iso_list = ['ARM', 'AZE', 'BLR', 'GEO', 'KAZ', 'KGZ', 'MDA', 'RUS', 'TJK', 'TKM', 'UKR', 'UZB'];
+    
+globagri_regions = ['CHN','SUN', 'BAL','YUG'];
+SUN_iso_list = ['ARM', 'AZE', 'BLR', 'GEO', 'KAZ', 'KGZ', 'MDA', 'RUS', 'TJK', 'TKM', 'UKR', 'UZB', 'EST', 'LVA', 'LTU','LTU','EST','LVA']
 BAL_iso_list = ['BEL', 'LUX'];
-# Note that Globagri has separate China regions, and SPAM has one CHN as aggregated entry
 CHN_iso_list = ['CHN', 'TWN', 'HKG']
 YUG_iso_list = ['BIH','HRV','MKD','MNE','SRB','SVN']
-SDN_iso_list = ['SDN','SSN']
 
 output_dir = '/Users/kristine/WRI/NationalGeographic/Phase2/Country_Bounds'
-country_bounds_f = '/Users/kristine/WRI/NationalGeographic/Phase2/Country_Bounds/CountryBounds.tif'
+country_bounds_f = '/Users/kristine/WRI/NationalGeographic/Phase2/Country_Bounds/g2008_0/g2008_0_iso.tif'
+iso_codes_df = pd.read_csv('/Users/kristine/WRI/NationalGeographic/Phase2/Country_Bounds/full_country_codes.csv')
+
 country_bounds_src = rasterio.open(country_bounds_f)
 country_bounds_nodata = country_bounds_src.nodatavals[0]
 data = country_bounds_src.read(1)
@@ -43,39 +39,25 @@ for region in globagri_regions:
         out_code = 156
     elif region == 'SUN':
         iso_list = SUN_iso_list
-        out_code = 810
+        out_code = 643
     elif region=='YUG':
         iso_list = YUG_iso_list
         out_code = 891
-    elif region=='SDN':
-        iso_list=SDN_iso_list
-        out_code = 729
     else:
         iso_list = BAL_iso_list
         out_code = 56
-    data = convert_iso_num(data,iso_list,out_code)
+    data = convert_iso_num(data,iso_list,iso_codes_df,out_code)
     
 
 kwds = country_bounds_src.profile
-out_local_file = os.path.join(output_dir,'GlobAgri_ISO.tif')
+out_local_file = os.path.join(output_dir,'GlobAgri_GAUL_ISO.tif')
 
 data[data<0] = 0
-#data[data==10] = 0
 print(np.unique(data))
-iso_codes_df = pd.read_csv('/Users/kristine/WRI/NationalGeographic/Phase2/FAO_country_codes_table.csv')
-iso_codes_df = np.unique(iso_codes_df[iso_codes_df['UNI'].isin(np.unique(data).tolist())]['ISO3'].values).tolist()
-print(iso_codes_df)
-cropping_intensity = pd.read_csv('/Users/kristine/WRI/NationalGeographic/Phase2/Crop_Livestock/cropping_intensity_country_all_scenarios.csv')
-cropping_intensity_iso = np.unique(cropping_intensity['ISO3'].values).tolist()
-common = set(cropping_intensity_iso) & set(iso_codes_df)
-different = [x for x in iso_codes_df+cropping_intensity_iso if x not in common]
-print(len(common))
-print(len(different))
-print(different)
-print(len(set(iso_codes_df)))
-print(sorted(common))
+kwds.update(dtype=rasterio.int16)
 with rasterio.open(out_local_file, 'w', **kwds) as dst_dataset:
     data = data.astype('Int16')
+    dst_dataset.dtype = 'Int16'
     dst_dataset.nodataval = 0
-    dst_dataset.write_band(1,data)
+    dst_dataset.write_band(1,data.astype('Int16'))
     
